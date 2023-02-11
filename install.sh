64cf1f46fb46b15cbcbc1b2f14cc6f27a6ed6678
VoidNoi <VoidNoi@users.noreply.github.com>
2023-02-11 22:29:20 +0100

    changed aur helper

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 cd ~
 
-#Installing yay AUR helper
-git clone https://aur.archlinux.org/yay.git
-cd yay
+#Installing paru AUR helper
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
 makepkg -si
 
 cd ..
@@ -16,12 +17,12 @@ sudo cp -r dotfiles/.config ~
 cp -r dotfiles/.screenlayout ~
 
 #Installing and theming terminal
-yay -S rxvt-unicode
+paru -S rxvt-unicode
 cp dotfiles/.Xresources ~
 cp -r dotfiles/.urxvt ~
 
 #Installing needed fonts
-yay -S noto-fonts siji-ttf noto-fonts-cjk ttf-font-awesome nerd-fonts-jetbrains-mono bdf-unifont
+paru -S noto-fonts siji-ttf noto-fonts-cjk ttf-font-awesome nerd-fonts-jetbrains-mono bdf-unifont
 
 #Installing bash autocompletion
 git clone --recursive https://github.com/akinomyoga/ble.sh.git
@@ -33,13 +34,13 @@ cp dotfiles/.git-prompt.sh ~
 
 #Installing programs I use
 sudo pacman -S micro nitrogen firefox rofi lutris neofetch virt-manager thunar samba gvfs-smb pavucontrol python-pip scrot imagemagick dunst zenity alsa-utils arandr mpv gimp seahorse polybar gnome-disk-utility arandr yarn unrar picom
-yay -S transmission-gtk file-roller ventoy-bin qemu-base ebtables steam vscodium timeshift 7-zip spotify-adblock
+paru -S transmission-gtk file-roller ventoy-bin qemu-base ebtables steam vscodium timeshift 7-zip spotify-adblock
 
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 #Setting papirus icons with catppuccin theme
-yay -S catppuccin-gtk-theme-macchiato papirus-icon-theme-git oranchelo-icon-theme-git
+paru -S catppuccin-gtk-theme-macchiato papirus-icon-theme-git oranchelo-icon-theme-git
 # sudo cp -r ~/dotfiles/usr ~
 
 git clone https://github.com/catppuccin/papirus-folders.git
@@ -66,5 +67,5 @@ sudo sed -i 's/user = "libvirt-qemu"/user = "$USER"/' /etc/libvirt/qemu.conf
 sudo sed -i 's/group = "libvirt-qemu"/group = "$USER"/' /etc/libvirt/qemu.conf
 
 sudo rm -r ~/ble.sh
-sudo rm -r ~/yay
+sudo rm -r ~/paru
 sudo rm -r ~/papirus-folders
